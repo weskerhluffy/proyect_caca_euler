19cec512dcb8f3c5d78f9ed252b4cec631708b74
ernesto.alvarado@oracle.com <ealvarad@slc11cmu.us.oracle.com>
2018-01-11 14:45:18 -0800

    se genera palo inf

--- src/pc/e4.py
+++ src/pc/e4.py
@@ -12,24 +12,25 @@ nivel_log = logging.DEBUG
 logger_cagada = None
 
 class numero_palote():
-    def __init__(cadena_num):
+    def __init__(self,cadena_num):
         assert len(cadena_num)==6
         assert cadena_num[0]!='0'
         assert int(cadena_num.strip())>101101
         self.cadena_num=cadena_num.strip()
         self.circulos=[]
         self.digitos_palo=None
+        self._inicializar()
         
     def _inicializar(self):
         self._init_circulos()
-	self.previo_palo()
+        self.previo_palo()
 
     def _init_circulos(self):
-        cadena=map(int,self.cadena_num)
+        cadena=list(map(int,self.cadena_num))
         cadena_cnt=len(cadena)
         offset_contraparte=cadena_cnt-1
         for i in range(cadena_cnt>>1):
-            self.circulos[i].append([cadena[i], cadena[i+offset_contraparte]])
+            self.circulos.append([cadena[i], cadena[i+offset_contraparte]])
             offset_contraparte-=2
 
     def son_circulos_palo(self):
@@ -43,31 +44,30 @@ class numero_palote():
     def previo_palo(self):
         se_pudo=True
         if not self.son_circulos_palo():
+            logger_cagada.debug("no era palo")
             circs=self.circulos
             circs_tam=len(circs)
-            idx_circulo_modificado=0
+            idx_circulo_modificado=-1
 
-            while idx_circulo_modificado<circs_tam:
+            for idx_circ in range(0,circs_tam):
+                if circs[idx_circ][0]!=circs[idx_circ][1]:
+                    idx_circulo_modificado = idx_circ
+            
+            if idx_circulo_modificado!=-1:
+                assert circs[idx_circulo_modificado][0]!=circs[idx_circulo_modificado][1]
+                if circs[idx_circulo_modificado][0]<circs[idx_circulo_modificado][1]:
+                    circs[idx_circulo_modificado][1]=circs[idx_circulo_modificado][0]
                 if circs[idx_circulo_modificado][0]>circs[idx_circulo_modificado][1]:
-                    break
-                circs[idx_circulo_modificado][1]=circs[idx_circulo_modificado][0]
-                idx_circulo_modificado+=1
-
-            if idx_circulo_modificado<circs_tam:
-                assert circs[idx_circulo_modificado][0]
-                circs[idx_circulo_modificado][0]-=1
-                circs[idx_circulo_modificado][1]=circs[idx_circulo_modificado][0]
-                idx_circulo_modificado+=1
-
-            while idx_circulo_modificado<circs_tam:
-                circs[idx_circulo_modificado][0]=9
-                circs[idx_circulo_modificado][1]=9
-                idx_circulo_modificado+=1
-
-            assert idx_circulo_modificado== circs_tam
+                    assert idx_circulo_modificado or circs[idx_circulo_modificado][0]>1
+                    circs[idx_circulo_modificado][0]-=1
+                    circs[idx_circulo_modificado][1]=circs[idx_circulo_modificado][0]
+                    for idx_circ in range(idx_circulo_modificado+1,circs_tam):
+                        circs[idx_circ][1]=circs[idx_circ][0]=9
+                for idx_circ in range(0,idx_circulo_modificado):
+                    circs[idx_circ][1]=circs[idx_circ][0]
 
             assert self.son_circulos_palo()
-            self.digitos_palo=map(lambda circ:circ[0], circs)
+            self.digitos_palo=list(map(lambda circ:circ[0], circs))
         else:
             digis=self.digitos_palo
             for idx_digi in range(len(digis)-1,-1,-1):
@@ -85,6 +85,7 @@ def pce_main():
     t = int(input().strip())
     for a0 in range(t):
         n = input().strip()
+        pce_core(n)
 
 if __name__ == "__main__":
     FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
